7d1ddf9db9999050cec28b9b939f50a96b458958
viry <vbatten10@hotmail.com>
2022-08-08 12:51:16 -0500

    Eliminar gasto

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,11 @@ function App() {
           setModal(false);
       }, 500);
   }
+
+  const eliminarGasto = id => {
+    const gastosActualizados = gastos.filter(gasto => gasto.id !== id);
+    setGastos(gastosActualizados);
+  }
   
   return (
     <div className={modal ? "fijar" : ""}>
@@ -68,6 +73,7 @@ function App() {
             <ListadoGastos 
               gastos={gastos}
               setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
             />
           </main>
           <div className='nuevo-gasto'>
